7e9f2d8074e4fd684a633b6e2628d895c273711c
danieldh206 <danieldh@uw.edu>
2021-02-20 15:31:54 -0800

    adding donors and new donations

--- mailroom.py
+++ mailroom.py
@@ -13,14 +13,17 @@ def donor_list():
     return ('\n'.join(names))
 
 def add_donor(name,amount):
-        
+
     name = name.strip()
-    for donor in db:
+    
+    for donor, donations in db:
         # Compare names in current list
-        if name.strip().lower() == donor[0].lower():
-            #print (donor[1])
-            return None
-             
+        if name.lower() == donor.lower():
+            donor = (donor,donations)
+            #print (donor[0])
+            #print (donations[0])
+            donations.append(amount)
+            return
         
     donor = (name, [amount])
     db.append(donor)
@@ -56,7 +59,7 @@ def main_selection():
 
 def letter(name,amount):
 
-    thank_you = (f'\nDear {name},\nThank you for your very kind donation of ${int(amount):.2f}. It will be put to very good use.\nSincerely,\n-The Team')
+    thank_you = (f'\nDear {name},\nThank you for your very kind donation of ${float(amount):.2f}. It will be put to very good use.\nSincerely,\n-The Team')
     print (thank_you)
     return thank_you
 
@@ -96,14 +99,16 @@ def donor_report():
         avg_gift = total_gifts / num_gifts
         report_rows.append((name, total_gifts, num_gifts, avg_gift))
         
-    print (report_rows)
+    #print (report_rows)
     # Sort the report data
     
     report_rows.sort(key=sort, reverse=True)
     
-    print (report_rows)
+    #print (report_rows)
     #Format report.
     report = []
+    report.append("\n" * 2)
+    report.append("-" * 66)
     report.append(f"{'Donor Name':25s} | {'Total Given':11s} | {'Num Gifts':9s} | {'Average Gifts':12s}")
     report.append("-" * 66)
     
@@ -112,6 +117,7 @@ def donor_report():
         report.append("{:25s}   {:11.2f}   {:9d}   {:12.2f}".format(*row))
 
     report = ("\n".join(report))
+    
     #print (report)
     return report
 
